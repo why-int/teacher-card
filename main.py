79f95dc8aed5898768f900b0333f3f313f9a4f89
i.odegov <i.odegov@b-resh.ru>
2026-03-11 07:57:37 +0500

    init repo

---
+++ main.py
@@ -0,0 +1,14 @@
+def main() -> None:
+    """Description.
+
+    Args:
+        arg: Description
+
+    Returns:
+        ReturnType: Description
+    """
+    pass
+
+
+if __name__ == "__main__":
+    main()
